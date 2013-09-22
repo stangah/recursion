9a995d3d9ee4b4ae8d121994c2f6a90cb7cd6fe0
stangah <pstanger@gmail.com>
2013-09-22 10:58:23 -0700

    First working function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,15 @@
 // };
 
 // But in stead we're going to implement it from scratch:
-var getElementsByClassName = function (className) {
+var getElementsByClassName = function (className, scope) {
   // your code here
+  var nodes = scope ? scope.childNodes : document.body.childNodes, ans = [];
+  for (var i = 0; i < nodes.length; i++) {
+  	if (nodes[i].classList !== undefined && nodes[i].classList.contains(className)) {
+  		ans.push(nodes[i]);
+  	} else if (nodes[i].childNodes.length > 0) {
+  		ans.push(getElementsByClassName(className, nodes[i]));
+  	}
+  }
+  return _.flatten(ans);
 };
